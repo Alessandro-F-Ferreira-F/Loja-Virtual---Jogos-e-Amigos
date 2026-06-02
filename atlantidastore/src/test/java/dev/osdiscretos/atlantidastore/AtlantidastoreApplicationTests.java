5f4ba21d436bab130ce8834c94083555eeacafc5
Red Grave <aleferjr@gmail.com>
2026-06-02 20:26:37 -0300

    Adiciona inicializador do Spring

---
+++ atlantidastore/src/test/java/dev/osdiscretos/atlantidastore/AtlantidastoreApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.osdiscretos.atlantidastore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtlantidastoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
